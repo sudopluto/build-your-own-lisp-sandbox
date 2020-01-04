1f167a9d37e35e4f2c64d8b3fa7d38f04b5b9ce2
Pranav Sharma <pranav.sharma.ama@gmail.com>
2020-01-03 19:40:30 -0500

    got the basic repl working

---
+++ src/sudolisp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+// will return string allocated on heap, need to delete
+std::string* console_getline(std::string prompt);
+
+#ifdef _WIN32
+std::string* console_getline(std::string prompt) {
+    std::cout << prompt;
+    std::string* line = new std::string;
+    std::getline(std::cin, *line);
+    return line;
+}
+
+#else
+
+#include <editline/readline.h>
+std::string* console_getline(std::string prompt) {
+    char* line = readline(prompt.c_str());
+    add_history(line);
+    std::string* ret = new std::string(line);
+    free(line);
+    return ret;
+}
+
+#endif
+
+int main(int argc, char** argv) {
+
+    bool run = true;
+    while(run) {
+        std::string* line = console_getline("sudolisp> ");
+        if (line->compare("quit") == 0) {
+            run = false;
+        }
+        else {
+            std::cout << "command not implemented: " << *line << std::endl;
+        }
+        delete line;
+    }
+
+    return 0;
+}
